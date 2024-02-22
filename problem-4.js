11f1db03b4b1e4bf2be641756fc4031eecc61c08
OHshajim <ajshajimmax@gmail.com>
2024-02-22 07:32:49 +0600

    using spread operator

---
+++ problem-4.js
@@ -0,0 +1,13 @@
+/**write an arrow function 
+ * 1. who take two array
+ * 2. add between 
+ * 3. give the largest element 
+ * 4. by using spread operator
+ */
+
+const large =(array1=[0] , array2=[0])=>{
+    const finalArray = [...array1,...array2]
+    console.log(finalArray);
+    return Math.max(...finalArray);
+};
+console.log(large([5,78,57,45],[4,-534,-0,45,53]));
