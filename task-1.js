724153da783f25e6cfe866b67744528042ba36d5
OHshajim <ajshajimmax@gmail.com>
2024-02-23 13:11:22 +0600

    add task-3

--- task-1.js
+++ task-1.js
@@ -11,17 +11,33 @@
  */
 const numbers = [1, 3, 5, 7, 9];
 const number = numbers.map(num => num + 1)
-console.log(number);
+// console.log(number);
 
 /**task-2
  * give  the number who divisible by 10
  * by using filter and find
  */
 
-// filter()
-const roll = [33, 50, 79, 78, 90, 101, 30]
-const Roll = roll.filter(num => num % 10 == 0);
-console.log(Roll);
 
-const rollFind = roll.find(num => num%10==0);
-console.log(rollFind);
+const roll = [33, 50, 79, 78, 90, 101, 30]
+
+// filter()
+const Roll = roll.filter(num => num % 10 == 0);
+// console.log(Roll);
+
+// find()
+const rollFind = roll.find(num => num % 10 == 0);
+// console.log(rollFind);
+
+
+/**Task-3
+ * give the name who senior
+ */
+const instructor = [
+    { name: 'Nodi', age: 18, position: 'Senior' },
+    { name: 'Akhi', age: 26, position: 'Junior' },
+    { name: 'Shobuj', age: 28, position: 'Senior' }
+];
+const Name = instructor.filter(id=> id.position==='Senior')
+const Names = Name.map(id=>id.name)
+console.log(Names);
